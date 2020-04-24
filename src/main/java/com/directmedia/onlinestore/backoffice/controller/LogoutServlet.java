c70b8de56dd180ed5c862c5744344c09865356b0
DaVv <davv09@gmail.com>
2020-04-24 14:38:36 +0200

    mise en place de la possibilité de déco du Panel admin

--- src/main/java/com/directmedia/onlinestore/backoffice/controller/LogoutServlet.java
+++ src/main/java/com/directmedia/onlinestore/backoffice/controller/LogoutServlet.java
@@ -5,25 +5,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HomeServlet", urlPatterns = {"/home"})
-public class HomeServlet extends HttpServlet {
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/logout"})
+public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         PrintWriter out = resp.getWriter();
+        req.getSession().invalidate();
         resp.setContentType("text/html;charset=UTF-8");
         out.print("<html>");
         out.print(" <head>");
-        out.print("<title>Back-Office HomeServlet</title>");
-        out.print("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+        out.print("<title>Deconnecté</title>");
         out.print("</head>");
         out.print("<body>");
-        out.print("Bonjour "+req.getSession().getAttribute("admin")+" <a href=\"logout\">(se deconnecter)</a><br/>");
-        out.print("<a href=\"add-work-form.html\">Ajouter une oeuvre au catalogue</a><br/>");
-        out.print("<a href=\"catalogue\">voir le  catalogue</a>");
+        out.print("Vous avez été déconnecté,cliquer <a href=\"login.html\">ici</a> pour vous reconnecter à nouveau.");
         out.print("</body>");
         out.print("</html>");
     }
